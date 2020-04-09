3b352a9b55cde173599148583f7a19027a2de0b6
jacobmilhon <58615265+jacobmilhon@users.noreply.github.com>
2020-04-08 19:26:23 -0500

    week 1 assignment

---
+++ week1/reactTodoAppPhase1/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+      <div>
+          <input type="checkbox" />
+          <p>First Task</p>
+          <input type="checkbox" />
+          <p>Second Task</p>
+          <input type="checkbox" />
+          <p>Third Task</p>
+          <input type="checkbox" />
+          <p>Fourth Task</p>
+      </div>
+  )
+};
+
+export default App;
